f3441e3f3b9ea78ac28fe7b7a92ae6076802ad69
Pedro Teodoro Goulart Santana <1008009@senac.educ>
2026-09-15 15:03:34 -0300

    tests: testa as funcionaidades de atividades

---
+++ testes/atividadeTestes.php
@@ -0,0 +1,60 @@
+
+
+<?php
+
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../models/Atividade.php';
+require_once __DIR__ . '/../Services/AtividadeService.php';
+
+$service = new AtividadeService($pdo);
+
+/*
+$atividade = new Atividade(
+    'Teste Atividade',
+    'teste descricao',
+    '2026-02-02',
+    '12:00',
+    '13:00',
+    'palacio',
+    12
+);
+
+if($service->cadastrarAtividade($atividade)){
+    echo "Atividade cadastrado com sucesso";
+}
+else{
+    echo "Erro";
+}
+
+*/
+
+$atividade = $service->listarAtividade();
+echo "<pre>";
+print_r($atividade);
+echo "<pre>";
+
+$atividade = $service->buscarAtividadePorId(2);
+echo "<pre>";
+print_r($atividade);
+echo "<pre>";
+
+$atividadeAtualizada = new Atividade(
+    'Atualizada',
+    'Atualizada',
+    '2028-02-10',
+    '15:00',
+    '19:00',
+    'ligar',
+    100
+);
+
+if($service->atualizarAtividade(1,$atividadeAtualizada)){
+    echo "Atividade Atualizada";
+}
+
+if($service->excluir(1)){
+    echo "Atividade excluida";
+}
+
+
+?>
