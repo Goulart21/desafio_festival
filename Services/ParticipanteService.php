4cf3db4f95785613156a190ae692164dbffd8227
Pedro Goulart <pedrotgs@icloud.com>
2026-09-17 16:28:03 -0300

    fix: concerta o comando sql

--- Services/ParticipanteService.php
+++ Services/ParticipanteService.php
@@ -48,7 +48,7 @@ class ParticipanteService
     public function listar(): array
     {
 
-        $sql = "SELECT * FROM participantes ORDER BY nome";
+        $sql = "SELECT * FROM participantes ORDER BY nome_participante";
 
         $stmt = $this->pdo->query($sql);
 
