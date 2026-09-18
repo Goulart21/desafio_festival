e494c6488aee9fc9eacd10d66fd8aa8ad4f10efd
Pedro Goulart <pedrotgs@icloud.com>
2026-09-18 15:16:41 -0300

    fix: corrige o cabeçalho

--- public/index.php
+++ public/index.php
@@ -52,25 +52,30 @@ $ocupacaoAtividade = $pdo->query(
                     <h1>Festival Experiência Viva</h1>
                 </a>
 
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="participantes.php">
-                            Participantes
-                        </a>
-                    </li>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavegacao"
+                    aria-controls="menuNavegacao" aria-expanded="false" aria-label="Abrir menu"><span class="navbar-toggler-icon"></span></button>
 
-                    <li class="navbar-nav ms-auto">
-                        <a class="nav-link" href="atividades.php">
-                            Atividades
-                        </a>
-                    </li>
+                <div class="collpse navbar-collapse" id="menuNavegacao">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="participantes.php">
+                                Participantes
+                            </a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="inscricoes.php" class="nav-link">
-                            Inscrições
-                        </a>
-                    </li>
-                </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="atividades.php">
+                                Atividades
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="inscricoes.php" class="nav-link">
+                                Inscrições
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
@@ -169,7 +174,7 @@ $ocupacaoAtividade = $pdo->query(
                 <div class="card-body">
 
                     <h2 class="h4 mb-4">Ocupação das atividades</h2>
-                    
+
                     <div class="table-responsive">
                         <table class="table table-hover align-middle">
                             <thead>
@@ -185,31 +190,31 @@ $ocupacaoAtividade = $pdo->query(
 
                                 <?php foreach ($ocupacaoAtividade as $atividade): ?>
 
-                                    <?php 
-                                        $percentual = $atividade['capacidade'] > 0
+                                    <?php
+                                    $percentual = $atividade['capacidade'] > 0
                                         ? ($atividade['inscritos'] / $atividade['capacidade']) * 100
                                         : 0;
-                                        ?>
+                                    ?>
 
-                                        <tr>
-                                            <td>
-                                                <?= htmlspecialchars($atividade['nome_atividade']) ?>
-                                            </td>
+                                    <tr>
+                                        <td>
+                                            <?= htmlspecialchars($atividade['nome_atividade']) ?>
+                                        </td>
 
-                                            <td>
-                                                <?= $atividade['inscritos'] ?>
-                                            </td>
+                                        <td>
+                                            <?= $atividade['inscritos'] ?>
+                                        </td>
 
-                                            <td>
-                                                <?= $atividade['capacidade'] ?>
-                                            </td>
+                                        <td>
+                                            <?= $atividade['capacidade'] ?>
+                                        </td>
 
-                                            <td>
-                                                <?= number_format($percentual, 0) ?>
-                                            </td>
-                                        </tr>
+                                        <td>
+                                            <?= number_format($percentual, 0) ?>
+                                        </td>
+                                    </tr>
 
-                                        <?php endforeach; ?>
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
